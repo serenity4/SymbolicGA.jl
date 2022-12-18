db759eeba98b2d2ebaf098d4eef0be8fccd72eed
serenity4 <cedric.bel@hotmail.fr>
2022-12-18 16:34:27 +0100

    Update small benchmark

--- benchmarks/operations.jl
+++ benchmarks/operations.jl
@@ -3,10 +3,13 @@ using BenchmarkTools
 
 x = (1.0, 2.0, 3.0)
 y = (50.0, 70.0, 70.0)
-@btime @ga 3 $x::Vector * $y::Vector
+
+# Constant propagation does all the work.
+@btime @ga 3 $x::Vector ⟑ $y::Vector
 
 function f(x, y)
-  @ga 3 x::Vector * y::Vector
+  @ga 3 Tuple x::Vector ⟑ y::Vector
 end
 
+@btime f(x, y)
 @code_typed f(x, y)
