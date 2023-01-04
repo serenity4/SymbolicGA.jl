88c8edf50f70d208522eaaf45548ec43b69ae304
serenity4 <cedric.bel@hotmail.fr>
2023-01-04 19:11:50 +0100

    Rotation benchmarks

--- benchmarks/operations.jl
+++ benchmarks/operations.jl
@@ -24,3 +24,36 @@ A = SMatrix([A₁ A₂ A₃ A₄])
 
 @btime (@ga 4 $A₁::Vector ∧ $A₂::Vector ∧ $A₃::Vector ∧ $A₄::Vector)[]
 @btime det($A)
+
+function rot(a, b, x, α)
+  @ga 3 begin
+    # Define unit plane for the rotation.
+    Π = a::Vector ⟑ b::Vector
+    # Define rotation generator.
+    Ω = exp((-α::Scalar / 2::Scalar) ⟑ Π)
+    # Apply the rotation by sandwhiching x with Ω.
+    Ω ⟑ x::Vector ⟑ reverse(Ω)
+  end
+end
+
+a = (1.0, 0.0, 0.0)
+b = (0.0, 1.0, 0.0)
+x = (1.0, 1.0, 0.0)
+α = π / 4
+
+@btime rot($a, $b, $x, $α)
+# @code_warntype rot(a, b, x, α)
+
+@time @macroexpand @ga 3 begin
+  # Define unit plane for the rotation.
+  Π = a::Vector ⟑ b::Vector
+  # Define rotation generator.
+  Ω = exp((-α::Scalar / 2::Scalar) ⟑ Π)
+  # Apply the rotation by sandwhiching x with Ω.
+  Ω ⟑ x::Vector ⟑ reverse(Ω)
+end;
+
+@time @macroexpand @ga 3 begin
+  Π = a::Vector ⟑ b::Vector
+  unitize(Π)
+end;
